091c0f66bc7e7736d825cb13be2c3fbd4d9a16dc
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2025-05-28 13:03:56 +0200

    yup

--- konyvtar.py
+++ konyvtar.py
@@ -8,13 +8,21 @@ class Konyvtar:
     def fajl_beolvasas(self):
         with open('konyvek.txt', 'r', encoding='utf-8') as forrasfajl:
             next(forrasfajl)
-        for sor in forrasfajl:
-            adatok = sor.strip().split(';')
-            nev = adatok[0]
-            szul_ev = int(adatok[1])
-            hal_ev = int(adatok[2])
-            nemzetiseg = adatok[3]
-            cim = adatok[4]
-            helyezes = int(adatok[5])
+            for sor in forrasfajl:
+                adatok = sor.strip().split(';')
+                nev = adatok[0]
+                szul_ev = int(adatok[1])
+                hal_ev = int(adatok[2]) if adatok[2] else 2005
+                nemzetiseg = adatok[3]
+                cim = adatok[4]
+                helyezes = int(adatok[5])
 
-            self.konyvek.append(Konyv(nev, szul_ev, hal_ev, nemzetiseg, cim, helyezes))
+                self.konyvek.append(Konyv(nev, szul_ev, hal_ev, nemzetiseg, cim, helyezes))
+
+    def konyvek_db(self):
+        len(self.konyvek)
+
+
+konyvtar = Konyvtar()
+for konyv in konyvtar.konyvek:
+    print(konyv.nev, konyv.cim)
